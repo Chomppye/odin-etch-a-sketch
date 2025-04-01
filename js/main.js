7c427c99330637bc401932ba1250a86ba1255fe3
Chomppye <202090366+Chomppye@users.noreply.github.com.>
2025-04-01 05:08:02 -0500

    add css to make the divs fill up the container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let divContainer = document.querySelector("#div-container")
 
 function createGrid() {
-    for (let i = 1; i < 3; i++) {
+    for (let i = 0; i < 256; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("Pink")
         divContainer.appendChild(newDiv)
