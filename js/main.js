81d0154ee2d832bd9404edabffc56bf1d5ed675c
Chomppye <202090366+Chomppye@users.noreply.github.com.>
2025-04-02 03:50:19 -0500

    add a button to change grid size from 64 to 256

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 let divContainer = document.querySelector("#div-container")
-let resetBtn = document.querySelector("#reset-button")
 
+let resetBtn = document.querySelector("#reset-button")
+let changeSizeBtn = document.querySelector("#change-size")
 
 function hover(event) {
     if (event.target.style.backgroundColor === "black") return;
     event.target.style.backgroundColor = "black"
+}
+
+function deleteGrid() {
+    let divs = divContainer.querySelectorAll(".Etch")
+    divs.forEach((div, index) => {
+        div.remove()
+    })
 }
 
 function createGrid(amount) {
@@ -12,7 +20,6 @@ function createGrid(amount) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("Etch")
         divContainer.appendChild(newDiv)
-
         newDiv.addEventListener("mouseover", hover)
     }
 }
@@ -24,6 +31,23 @@ function resetGrid() {
     })
 }
 
+function changeGridSize(event) {
+    let value = prompt("Change the amount of squares in the grid, Max = 256, min = 64")
+    value = parseInt(value)
+
+    if (!isNaN(value) && value >= 64 && value <= 256) {
+        deleteGrid()
+        createGrid(value)
+    } else {
+        alert("Incorrect input, please enter a number between 64 and 256")
+    }
+}
+
+function test() {
+    console.log("ggg")
+}
+
 createGrid(256)
 
 resetBtn.addEventListener("click", resetGrid)
+changeSizeBtn.addEventListener("click", changeGridSize)
